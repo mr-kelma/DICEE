7531872948c386e5f6a83821b6425be1144a08cf
Valery Keplin <98012564+mr-kelma@users.noreply.github.com>
2022-06-02 16:47:18 +0400

    Downloading the complete project

--- DICEE/ViewController.swift
+++ DICEE/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+let arrayOfDice = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImageViewLeft: UIImageView!
+    @IBOutlet weak var diceImageViewRight: UIImageView!
+    
+    override var shouldAutorotate: Bool {
+        false
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageViewLeft.image = UIImage(imageLiteralResourceName: "DiceOne")
+        diceImageViewRight.image = #imageLiteral(resourceName: "DiceOne")
+        //different recording options "UIImage(imageLiteralResourceName: "DiceFive")" or "#imageLiteral()"
+        (diceImageViewLeft.alpha, diceImageViewRight.alpha) = (0.0, 0.0)
     }
-
-
+    
+    @IBAction func trowButtonPressed(_ sender: UIButton) {
+        diceImageViewLeft.image = arrayOfDice.randomElement()
+        diceImageViewRight.image = arrayOfDice[Int.random(in: 0...5)]
+        //different recording options '[Int.random(in: _..._)]' or 'randomElement()'
+        (diceImageViewLeft.alpha, diceImageViewRight.alpha) = (1.0, 1.0)
+        //makes dices isible after throw
+    }
 }
-
